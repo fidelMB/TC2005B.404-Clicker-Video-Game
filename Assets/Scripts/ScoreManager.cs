f05ffaed0b86880327a9867a85106f86f5d30a7e
Fidel Morales <fidel.morales.briones@gmail.com>
2024-04-11 15:59:58 -0600

    Cambio en UI y upgrades
    Las upgrades ya tienen disponible la mejora de el poder del click.

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.UIElements;
 
 public class ScoreManager : MonoBehaviour
 {
-    public Text neoCoinsText;
+    public TMP_Text neoCoinsText;
     public float neoCoins;
     public float clickValue;
     public float autoClickValue;
@@ -17,14 +18,14 @@ public class ScoreManager : MonoBehaviour
         // Estos valores están de ejemplo, pero se van a cargar desde el archivo de guardado de la base de datos
         neoCoins = 0;
         clickValue = 1;
-        autoClickValue = 1;   
+        autoClickValue = 0;   
     }
 
     // Update is called once per frame
     void Update()
     {
         // Actualiza el texto de las monedas
-        neoCoinsText.text = "Neo Coins: " + (int)neoCoins;
+        neoCoinsText.text = "<sprite name=neo_coin>" + (int)neoCoins;
 
         // Neo coins ganadas por segundo en el autoclick
         neoCoins += autoClickValue * Time.deltaTime;
--- Assets/Scripts/UpgradeManager.cs
+++ Assets/Scripts/UpgradeManager.cs
@@ -8,17 +8,19 @@ public class UpgradeManager : MonoBehaviour
     // Referencia al ScoreManager para poder acceder a las variables dentro de él
     [SerializeField] private ScoreManager ScoreManager;
     public int upgradeCost;
-    public float upgradeValue;
+    public float upgradeIncrement;
     public string upgradeName;
     public int upgradeLevel;
-    public float upgradeValueIncrementAdder;
+    public float upgradeIncrementAdder;
     public Text nameLvlCoinsPerSecText;
     public Text upgradeCostButtonText;
+    public string upgradeType;
+    //public string upgradeCostCoin;
 
     // Start is called before the first frame update
     void Start()
     {
-        nameLvlCoinsPerSecText.text = upgradeName + " Lvl " + upgradeLevel + "\nSiguiente mejora: " + upgradeValue + " Neo Coins /s";
+        nameLvlCoinsPerSecText.text = upgradeName + " Lvl " + upgradeLevel + "\nSiguiente mejora: " + upgradeIncrement + " Neo Coins /s";
         upgradeCostButtonText.text = "COMPRAR " + upgradeCost + " Neo Coins";
     }
 
@@ -35,19 +37,33 @@ public class UpgradeManager : MonoBehaviour
         if (ScoreManager.neoCoins >= upgradeCost)
         {
 
+            if (upgradeType == "Auto Click")
+            {
+                // Actualizar monedas, valor del autoclick, coste del upgrade y nivel del upgrade
+                ScoreManager.neoCoins -= upgradeCost;
+                upgradeCost = (int)(upgradeCost * 2.5f);
+                upgradeLevel++;
+                upgradeIncrement = upgradeIncrement + upgradeIncrementAdder;
+                ScoreManager.autoClickValue += upgradeIncrement;
+                // Actualizar texto de monedas por segundo
+                nameLvlCoinsPerSecText.text = upgradeName + " Lvl " + upgradeLevel + "\nSiguiente mejora: " + upgradeIncrement + " Neo Coins /s";
+            }
+            else if (upgradeType == "Click Power")
+            {
+                // Actualizar monedas, valor del autoclick, coste del upgrade y nivel del upgrade
+                ScoreManager.neoCoins -= upgradeCost;
+                upgradeCost = (int)(upgradeCost * 2.5f);
+                upgradeLevel++;
+                upgradeIncrement = upgradeIncrement + upgradeIncrementAdder;
+                ScoreManager.clickValue = upgradeIncrement;
 
-            // Actualizar monedas, valor del autoclick, coste del upgrade y nivel del upgrade
-            ScoreManager.neoCoins -= upgradeCost;
-            upgradeCost = (int)(upgradeCost * 2.5f);
-            upgradeLevel++;
-            upgradeValue = upgradeValue + upgradeValueIncrementAdder;
-            ScoreManager.autoClickValue += upgradeValue;
-
+                // Actualizar monedas, valor del click, coste del upgrade y nivel del upgrade
+                nameLvlCoinsPerSecText.text = upgradeName + " Lvl " + upgradeLevel + "\nSiguiente mejora: " + upgradeIncrement + " Neo Coins /s";
+            }
 
             // Actualizar texto del botón, nivel y 
             upgradeCostButtonText.text = "COMPRAR " + upgradeCost + " Neo Coins";
-            // Actualizar texto de monedas por segundo
-            nameLvlCoinsPerSecText.text = upgradeName + " Lvl " + upgradeLevel + "\nSiguiente mejora: " + upgradeValue + " Neo Coins /s";
+            
         }
     }
 }
