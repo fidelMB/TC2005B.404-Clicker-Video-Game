bf6083a32131101d24d6517b55471ecc1fd4fb0b
Felipe <a00837590@tec.mx>
2024-04-23 09:06:41 -0600

    avance (lo perdido en la crisis de github)

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -8,6 +9,8 @@ using UnityEngine.UIElements;
 public class ScoreManager : MonoBehaviour
 {
     public TMP_Text neoCoinsText; // Texto que muestra las monedas
+    public TMP_Text neoCoinsPerSec; // Texto que muestra monedas por segundo
+    public TMP_Text neoCoinsPerClick; // Texto que muestra monedas por click
     public float neoCoins; // Monedas
     public float clickValue; // Valor de las monedas ganadas por click
     public float autoClickValue; // Valor de las monedas ganadas por segundo
@@ -18,7 +21,7 @@ public class ScoreManager : MonoBehaviour
         // Estos valores están de ejemplo, pero se van a cargar desde el archivo de guardado de la base de datos
         neoCoins = 200;
         clickValue = 1;
-        autoClickValue = 0;   
+        autoClickValue = 0;
     }
 
     // Update is called once per frame
@@ -26,10 +29,11 @@ public class ScoreManager : MonoBehaviour
     {
         // Actualiza el texto de las monedas
         neoCoinsText.text = "<sprite name=neo_coin>" + (int)neoCoins;
+        neoCoinsPerSec.text = ((int)autoClickValue).ToString() + "<sprite name=neo_coin>/s";
+        neoCoinsPerClick.text = ((int)clickValue).ToString() + "<sprite name=neo_coin>/click";
 
         // Neo coins ganadas por segundo en el autoclick
         neoCoins += autoClickValue * Time.deltaTime;
-
     }
 
     // Método para añadir monedas al hacer click
