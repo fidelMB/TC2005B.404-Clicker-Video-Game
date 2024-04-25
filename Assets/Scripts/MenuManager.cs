913273c5069d2619593e199a2c0b6374e6945489
Felipe <a00837590@tec.mx>
2024-04-24 19:28:36 -0600

    avance animacion y tienda avatar
    tienda sombreros y camisas (falta mascotas)

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -7,9 +7,12 @@ public class MenuManager : MonoBehaviour
     // Variables para los paneles de tienda, avatar, ranking y cursos
     public GameObject newPanel1;
     public GameObject newPanel2;
+    public GameObject newPanel3;
+    public GameObject newPanel4;
     public GameObject oldPanel1;
     public GameObject oldPanel2;
     public GameObject oldPanel3;
+    public GameObject oldPanel4;
 
     // Dentro de unity, se asigna a cada botón de la barra de navegación el panel que se quiere abrir
     // como newPanel y los otros tres paneles como oldPanel1, oldPanel2 y oldPanel3
@@ -18,9 +21,12 @@ public class MenuManager : MonoBehaviour
         Debug.Log("OpenPanel called");
         if (newPanel1 != null) newPanel1.SetActive(true);
         if (newPanel2 != null) newPanel2.SetActive(true);
+        if (newPanel3 != null) newPanel3.SetActive(true);
+        if (newPanel4 != null) newPanel4.SetActive(true);
         if (oldPanel1 != null) oldPanel1.SetActive(false); // Check if the object is not null before setting active state
         if (oldPanel2 != null) oldPanel2.SetActive(false);
         if (oldPanel3 != null) oldPanel3.SetActive(false);
+        if (oldPanel4 != null) oldPanel4.SetActive(false);
     }
 
 }
